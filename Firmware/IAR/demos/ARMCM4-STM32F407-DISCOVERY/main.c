5e4dddbff4f96960b808094f5ecb401a86a856a5
Alex <alex.borysevych@gmail.com>
2018-07-15 23:56:31 -0700

    additional configuartion options for start/stop logs implemented
    start_on_power, circular_write, start_id, stop_id, start_on_CAN, stop_on_CAN, start_frame_to_name

--- Firmware/IAR/demos/ARMCM4-STM32F407-DISCOVERY/main.c
+++ Firmware/IAR/demos/ARMCM4-STM32F407-DISCOVERY/main.c
@@ -26,7 +26,8 @@
 #include "file_utils.h"
 #include <time.h>
 
-
+#include <stdio.h>
+#include <limits.h>
 
 //#define INDICATE_IDLE_ON() palSetPad(GPIOA, GPIOA_PIN5_LED_R)
 //#define INDICATE_IDLE_OFF() palClearPad(GPIOA, GPIOA_PIN5_LED_R)
@@ -149,12 +150,23 @@ char sLine[STRLINE_LENGTH];
 systime_t stLastWriting;
 unsigned char bIncludeTimestamp = 1;
 
-void start_log()
+void start_log(int file_index, char* file_name)
 {
   // open file and write the begining of the load
-  rtcGetTimeTm(&RTCD1, &timp);        
-  sprintf(sLine, "%02d-%02d-%02d.csv", timp.tm_hour, timp.tm_min, timp.tm_sec); // making new file
-
+  if (file_index >= 0)
+  {
+    sprintf(sLine, "%d.csv", file_index);
+  }
+  else if (file_name != 0)
+  {
+    sprintf(sLine, "%s.csv", file_name);
+  }
+  else
+  {
+    rtcGetTimeTm(&RTCD1, &timp);        
+    sprintf(sLine, "%02d-%02d-%02d.csv", timp.tm_hour, timp.tm_min, timp.tm_sec);
+  }
+  
   file = fopen_(sLine, "a");
   if (bIncludeTimestamp)
     strcpy(sLine, "Timestamp,ID,Data0,Data1,Data2,Data3,Data4,Data5,Data6,Data7\r\n");
@@ -181,10 +193,24 @@ int iFilterValue = 0;
 unsigned char bLogStdMsgs = 1;
 unsigned char bLogExtMsgs = 1;
 
+unsigned char bAutoStart = 0;
+unsigned char bCircularWriting = 0;
+unsigned char bStartOnCAN = 0;
+unsigned char bStopOnCAN = 0;
+int iIDStartValue = 0;
+int iIDStartMask = 0;
+unsigned char bStartFrameToName = 0;
+int iIDStopValue = 0;
+int iIDStopMask = 0;
+
+int iLastFileName = 0; // the file name index from file 
+
+
 int read_config_file()
 {
   int value;
   char name[128];
+  char param[128];
   int baud;
   int res = 0;
   int ack = 0;
@@ -205,46 +231,123 @@ int read_config_file()
   
   while( f_gets(sLine, STRLINE_LENGTH, file) )
   {
-    if (sscanf(sLine, "%s %d", name, &value) == 0)
+    if (sscanf(sLine, "%s %s", name, param) == 0)
     {
       continue;
     }
     
     if (strcmp(name, "baud") == 0)
     {
+      if (sscanf(param, "%d", &value) == 0) continue;
+
       baud = value;
       res = 1; // at least we got baudrate, config file accepted
     }
     else
     if (strcmp(name, "ack_en")  == 0)
     {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
       ack = value;
     }
     else
     if (strcmp(name, "id_filter_mask")  == 0)
     {
+      if (sscanf(param, "%x", &value) == 0) continue;
+      
       iFilterMask = value;
     }
     else
     if (strcmp(name, "id_filter_value")  == 0)
     {
+      if (sscanf(param, "%x", &value) == 0) continue;
+      
       iFilterValue = value;
     }
     else
     if (strcmp(name, "timestamp")  == 0)
     {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
       bIncludeTimestamp = value;
     }
     else
     if (strcmp(name, "log_std")  == 0)
     {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
       bLogStdMsgs = value;
     }
     else
     if (strcmp(name, "log_ext")  == 0)
     {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
       bLogExtMsgs = value;
-    }    
+    }
+    else
+    if (strcmp(name, "start_on_power")  == 0)
+    {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
+      bAutoStart = value;
+    }
+    else
+    if (strcmp(name, "circular_write")  == 0)
+    {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
+      bCircularWriting = value;
+    }
+    else
+    if (strcmp(name, "start_id_value")  == 0)
+    {
+      if (sscanf(param, "%x", &value) == 0) continue;
+      
+      iIDStartValue = value;
+    }
+    else
+    if (strcmp(name, "start_id_mask")  == 0)
+    {
+      if (sscanf(param, "%x", &value) == 0) continue;
+      
+      iIDStartMask = value;
+    }
+    else
+    if (strcmp(name, "start_frame_to_name")  == 0)
+    {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
+      bStartFrameToName = value;
+    }
+    else
+    if (strcmp(name, "stop_id_value")  == 0)
+    {
+      if (sscanf(param, "%x", &value) == 0) continue;
+      
+      iIDStopValue = value;
+    }
+    else
+    if (strcmp(name, "stop_id_mask")  == 0)
+    {
+      if (sscanf(param, "%x", &value) == 0) continue;
+      
+      iIDStopMask = value;
+    }
+    else
+    if (strcmp(name, "start_on_CAN")  == 0)
+    {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
+      bStartOnCAN = value;
+    }
+    else
+    if (strcmp(name, "stop_on_CAN")  == 0)
+    {
+      if (sscanf(param, "%d", &value) == 0) continue;
+      
+      bStopOnCAN = value;
+    }
   }
   
   // configure CAN
@@ -359,6 +462,59 @@ int read_playback_file()
   return 1;
 }
 
+int iMinFileName = INT_MAX;
+int iMaxFileName = -1;
+
+FRESULT scan_files (char* path)
+{
+    FRESULT res;
+    DIR dir;
+    //UINT i;
+    static FILINFO fno;
+    static char lfname[256];
+    char name[128];
+    char ext[64];
+    int iFileName = 0;
+
+
+    res = f_opendir(&dir, path);                       /* Open the directory */
+    if (res == FR_OK) 
+    {
+        for (;;) 
+        {
+            res = f_readdir(&dir, &fno);                   /* Read a directory item */
+            if (res != FR_OK || fno.fname[0] == 0) break;  /* Break on error or end of dir */
+            if (fno.fattrib & AM_DIR) 
+            {                    
+              /* It is a directory */
+            } 
+            else 
+            {                                       
+                /* It is a file. */
+                if (sscanf(fno.fname, "%[^.].%s", name, ext) != 0)
+                {
+                  // we can separate file name into name.ext
+                  if (strcmp(ext, "csv") == 0)
+                  {
+                    // if it is csv file
+                    if (sscanf(name, "%d", &iFileName) != 0)
+                    {
+                      // the name is a digit
+                      if (iFileName > iMaxFileName) iMaxFileName = iFileName;
+                      if (iFileName < iMinFileName) iMinFileName = iFileName;
+                    }
+                  }
+                }
+                //printf("%s/%s\n", path, fno.fname);
+            }
+        }
+        //f_closedir(&dir);
+    }
+
+    return res;
+}
+
+
 int init_sd()
 {
   // initializing SDC interface
@@ -394,6 +550,9 @@ static const CANConfig cancfg = {
 };
 */
 
+// this is used only when start_frame_to_name=1 for translate CAN message to file name
+char sNewFileName[128] = "";
+
 char sTmp[128];
 
 static WORKING_AREA(can1_rx_wa, 256);
@@ -412,6 +571,25 @@ static msg_t can1_rx(void *p) {
     while (canReceive(&CAND2, CAN_ANY_MAILBOX, &rxmsg, TIME_IMMEDIATE) == RDY_OK) 
     {
       /* Process message.*/
+      
+      if (!bLogging && bStartOnCAN)
+      {
+        // should we start?
+        if (rxmsg.IDE == 0) // if STD then just copy standard to extended ID
+          rxmsg.EID = rxmsg.SID;
+
+        // then check filter conditions
+        if ((rxmsg.EID & iIDStartMask) == (iIDStartValue & iIDStartMask)) 
+        {
+          bButton = 1; // TODO: this one is very dirty way to start a log :)
+          if (bStartFrameToName)
+          {
+            sNewFileName[0] = 0;
+            for (i = 0; i < rxmsg.DLC; i++)
+              sprintf(sNewFileName+strlen(sNewFileName), "%01X", (rxmsg.data8[i])&0xF);
+          }
+        }
+      }
       
       if (bLogging)
       {
@@ -436,7 +614,7 @@ static msg_t can1_rx(void *p) {
           // then check filter conditions
           if ((rxmsg.SID & iFilterMask) != (iFilterValue & iFilterMask)) continue;
 		  
-          rxmsg.EID = rxmsg.SID;
+          rxmsg.EID = rxmsg.SID; // this is because we are writing value from extended ID
         }
 
         // write down data
@@ -452,6 +630,21 @@ static msg_t can1_rx(void *p) {
 
         strcat(sTmp, "\r\n");
         fwrite_string(sTmp);
+        
+        // should we stop?
+        if (bStopOnCAN)
+        {
+          if (rxmsg.IDE == 0) // if STD then just copy standard to extended ID
+            rxmsg.EID = rxmsg.SID;
+
+          // then check filter conditions
+          if ((rxmsg.EID & iIDStopMask) == (iIDStopValue & iIDStopMask)) 
+          {
+            bLogging = 0;
+            // we are in logging state -- should write the rest of the log
+            request_write();
+          }
+        }
       }
       
       palTogglePad(GPIOA, GPIOA_PIN7_LED_G);
@@ -467,7 +660,7 @@ unsigned char bButtonNew = 0;
 unsigned char bButtonPrev = 0;
 #define BUTTON_COUNTER_THRESHOLD 50000
 
-
+char sName[128];
 /*
  * Application entry point.
  */
@@ -484,13 +677,29 @@ int main(void)
   canStart(&CAND1, &cancfg);
   canStart(&CAND2, &cancfg);
   chThdCreateStatic(can1_rx_wa, sizeof(can1_rx_wa), NORMALPRIO + 7, can1_rx, NULL);
-  /*
-  while (1)
+
+
+  // first -- let us try to open SD card, maybe there is something interesting there
+  if (init_sd()) // trying to initialize sd card
   {
-    canTransmit(&CAND2, CAN_ANY_MAILBOX, &txmsg, MS2ST(100));
-    chThdSleepMilliseconds(50);
+    if (read_config_file()) // trying to read configuration file
+    {
+      // maybe there is playback mode?
+      if (bAutoStart)
+        read_playback_file();
+      
+      // scanning files to determine range of file names 
+      scan_files("");
+      
+      // start loging if we need to do it after power on
+      if (bAutoStart)
+      {
+        // iMaxFileName
+        iMaxFileName++;
+        start_log(iMaxFileName, 0);
+      }
+    }
   }
-  */
 
   i = 0;
   while (TRUE) 
@@ -511,6 +720,30 @@ INDICATE_IDLE_ON();
     {
       //palSetPad(GPIOD, GPIOD_PIN_15_BLUELED);
 INDICATE_IDLE_OFF();
+      if (bCircularWriting)
+      {
+        // we need to check free space and delete old files
+        FATFS *fs;
+        DWORD fre_clust, fre_sect, tot_sect;
+
+        fs = &SDC_FS;
+        if (f_getfree(0, &fre_clust, &fs) == 0)
+        {
+          tot_sect = (fs->n_fatent - 2) * fs->csize;
+          fre_sect = fre_clust * fs->csize;
+          
+          // if disk is close to full
+          if (fre_sect/((float)(tot_sect)) < 0.1F)
+          {
+            // erase one old file
+            sprintf(sName, "%d.csv", iMinFileName);
+            f_unlink(sName);
+            iMinFileName++;
+          }
+        }
+      }
+      
+
       if (fwrite_(sd_buffer_for_write, 1, sd_buffer_length_for_write, file) != sd_buffer_length_for_write)
         bWriteFault = 2;
       if (f_sync(file) != FR_OK)
@@ -532,6 +765,8 @@ INDICATE_IDLE_ON();
         
         // we are in logging state -- should write the rest of log
         request_write();
+        
+        if (bStartFrameToName) strcpy(sNewFileName, ""); // empty file name
       }
       else
       {
@@ -543,11 +778,29 @@ INDICATE_IDLE_ON();
         {
           if (read_config_file()) // trying to read configuration file
           {
-            // maybe there is playback mode?
-            read_playback_file();
+            if (bStartOnCAN == 0) // we can skip this in bStartOnCAN mode
+            {
+              // maybe there is playback mode?
+              read_playback_file();            
+              // scanning files to determine range of file names 
+              scan_files("");
+            }
             
-            // all done -- start loging
-            start_log();
+            // all done -- should start loging
+            
+            // if we are using CAN frame as name
+            if (bStartFrameToName && sNewFileName[0] != 0) 
+            {
+              // file name in sNewFileName should be stored before
+              start_log(-1, sNewFileName); 
+            }
+            else
+            {
+              // iMaxFileName -- used as file name
+              iMaxFileName++;
+              start_log(iMaxFileName, 0);
+            }
+
           }
           else
           {
